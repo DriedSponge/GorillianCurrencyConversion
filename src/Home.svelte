08a9e3682afdcfa4aef2211eff8726858b12c819
DriedSponge <jordan@driedsponge.net>
2021-04-30 00:05:21 -0700

    Updated font

--- src/Home.svelte
+++ src/Home.svelte
@@ -7,31 +7,28 @@
     ]
     let convertingFrom;
     let convertingTo =  {name:"USD",sym:"$",rate:3};
-    let convertFrom = 0.00;
+    let convertFromVal = 0.00;
     let convertedAmt = 0.00
-    function convert(){
-        console.log("test")
-    }
 </script>
 <main>
     <div class="container max-w-6xl mx-auto px-4 pt-5">
         <div class="container bg-white rounded-lg py-5 px-3 w-full shadow-2xl">
             <h1 class="lg:text-4xl text-2xl font-bold text-center mb-3">{name}</h1>
             <br>
-            <h2 class="text-center font-bold text-lg lg:text-xl mb-3">Converting from Gorillian to {convertingTo}</h2>
+            <h2 class="text-center font-bold text-lg lg:text-xl mb-3">Converting from Gorillian to {convertingTo.name}</h2>
             <div class="flex flex-row  flex-wrap md:flex-nowrap">
                 <div class="mx-1 w-full my-2">
-                    <input id="input" bind:value={convertFrom} on:change={convert()} placeholder="0.00" type="number"/>
+                    <input id="input" bind:value={convertFromVal} placeholder="0.00" type="number"/>
                     <div class="w-full">
-                        <select disabled>
-                                <option>Gorillian Dollars</option>
+                        <select disabled class="appearance-none">
+                                <option>🍌 Gorillian Dollars</option>
                         </select>
                     </div>
                 </div>
-                <span class="text-center select-none font-bold w-full my-1 text-2xl align-middle md:hidden">&#8645;</span>
+                <span class="text-center w-full select-none font-bold my-1 text-2xl align-middle md:hidden">&#8645;</span>
                 <span class="text-center select-none font-bold align-middle mx-2 my-auto text-xl hidden md:inline-block" >&#8644;</span>
                 <div class="mx-1 w-full my-2">
-                    <input id="output" value={convertFrom * convertingTo.rate} placeholder="0.00"/>
+                    <input id="output" disabled value={convertFromVal * convertingTo.rate} placeholder="0.00"/>
                     <div class="w-full">
                         <select bind:value={convertingTo}>
                             {#each currencies as currency}
@@ -41,14 +38,17 @@
                     </div>
                 </div>
             </div>
-            <p class="text-center mt-4 text-gray-300 font-bold italic select-none text-sm md:text-md">In Gorilla We Trust &bull;
+            <p class="text-center mt-4 text-gray-300 font-bold italic select-none text-sm md:text-md">In Banana We Trust &bull;
                 <a class="underline text-gray-300 hover:text-gray-300 visited:text-gray-300" href="https://github.com/DriedSponge/GorillianCurrencyConversion" target="_blank">GitHub</a></p>
         </div>
     </div>
 </main>
 <style lang="postcss" global>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
     body {
         background-color: #0E1013;
+        font-family: Roboto, sans-serif;
     }
 
     select, input {
