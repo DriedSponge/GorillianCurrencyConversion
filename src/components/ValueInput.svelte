f1e1475740c0808e4ef0558f3c63db424f341b1e
DriedSponge <jordan@driedsponge.net>
2021-05-16 18:57:55 -0700

    Added unversioned file

---
+++ src/components/ValueInput.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let place = "Enter value...";
+</script>
+<label for="input" class="hidden">Enter amount of gorillian dollars</label>
+<input id="input" class="rounded-t-lg rounded-b-none border-gray-100 border-2 active:border-blue-200"  placeholder="{place}" type="number"/>
+
+<style lang="postcss">
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
