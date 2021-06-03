d68d61c5baeff39da7e870fdccf93bb5997eeb1f
DriedSponge <jordan@driedsponge.net>
2021-06-03 13:49:33 -0700

    Added og:title

--- src/src/routes/index.svelte
+++ src/src/routes/index.svelte
@@ -21,7 +21,8 @@
     let description = "Here at Gorilla "
 </script>
 <svelte:head>
-    <title>Gorillian Dollar Converter</title>
+    <title>Gorillian Unit Converter - Dollars</title>
+    <meta property="og:title" content="Gorillian Unit Converter - Dollars">
     <meta property="og:description" content="Our mission is to unite global markets under one unit, the Gorillian unit. Our experts have meticulously poked and prodded at our units foundation, ensuring it's perfection and sustainability for the global economy.">
     <meta property="description" content="Our mission is to unite global markets under one unit, the Gorillian unit. Our experts have meticulously poked and prodded at our units foundation, ensuring it's perfection and sustainability for the global economy.">
 </svelte:head>
