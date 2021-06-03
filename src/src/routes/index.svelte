79beb1c09d5927ebe287df536c85793e36b1797c
DriedSponge <jordan@driedsponge.net>
2021-06-02 23:33:34 -0700

    Updated description

--- src/src/routes/index.svelte
+++ src/src/routes/index.svelte
@@ -22,8 +22,8 @@
 </script>
 <svelte:head>
     <title>Gorillian Dollar Converter</title>
-    <meta property="og:description" content="">
-    <meta property="description" content="Convert">
+    <meta property="og:description" content="Our mission is to unite global markets under one unit, the Gorillian unit. Our experts have meticulously poked and prodded at our units foundation, ensuring it's perfection and sustainability for the global economy.">
+    <meta property="description" content="Our mission is to unite global markets under one unit, the Gorillian unit. Our experts have meticulously poked and prodded at our units foundation, ensuring it's perfection and sustainability for the global economy.">
 </svelte:head>
 <br>
 <h2 class="text-center font-bold text-lg lg:text-xl mb-3">Converting from {swapped ? convertingTo.name : "Gorillian"}
